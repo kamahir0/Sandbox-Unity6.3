84ce73d89c9be64d5e0a548d1dc1392b378408ff
kamahir0 <hrtk1330@gmail.com>
2026-03-18 00:01:47 +0900

    ヘッダーにバックボタン追加

--- Assets/lilja.debug-menu/src/Scripts/DebugMenuFrame.cs
+++ Assets/lilja.debug-menu/src/Scripts/DebugMenuFrame.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine.UIElements;
 
 namespace Lilja.DebugMenu
@@ -12,15 +13,22 @@ namespace Lilja.DebugMenu
     [UxmlElement]
     public partial class DebugMenuFrame : VisualElement
     {
-        public static readonly string ussClassName        = "c-window";
-        public static readonly string headerUssClassName  = ussClassName + "__header";
-        public static readonly string titleUssClassName   = ussClassName + "__title";
-        public static readonly string contentUssClassName = ussClassName + "__content";
+        public static readonly string ussClassName             = "c-window";
+        public static readonly string headerUssClassName       = ussClassName + "__header";
+        public static readonly string backButtonUssClassName     = ussClassName + "__back-button";
+        public static readonly string backButtonIconUssClassName = ussClassName + "__back-button-icon";
+        public static readonly string headerSpacerUssClassName   = ussClassName + "__header-spacer";
+        public static readonly string titleUssClassName        = ussClassName + "__title";
+        public static readonly string contentUssClassName      = ussClassName + "__content";
 
+        private readonly Button        _backButton;
         private readonly Label         _titleLabel;
         private readonly VisualElement _contentContainer;
 
         public override VisualElement contentContainer => _contentContainer;
+
+        /// <summary>バックボタンがクリックされたときに発火するイベント。</summary>
+        public event Action BackClicked;
 
         [UxmlAttribute]
         public string label
@@ -40,9 +48,23 @@ namespace Lilja.DebugMenu
             header.AddToClassList(headerUssClassName);
             hierarchy.Add(header);
 
+            _backButton = new Button(() => BackClicked?.Invoke());
+            _backButton.AddToClassList(backButtonUssClassName);
+            var backButtonIcon = new VisualElement();
+            backButtonIcon.AddToClassList(backButtonIconUssClassName);
+            backButtonIcon.pickingMode = PickingMode.Ignore;
+            _backButton.Add(backButtonIcon);
+            header.Add(_backButton);
+
             _titleLabel = new Label(label);
             _titleLabel.AddToClassList(titleUssClassName);
             header.Add(_titleLabel);
+
+            // バックボタンと同幅のスペーサーでタイトルを視覚的に中央寄せ
+            var spacer = new VisualElement();
+            spacer.AddToClassList(headerSpacerUssClassName);
+            spacer.pickingMode = PickingMode.Ignore;
+            header.Add(spacer);
 
             _contentContainer = new VisualElement();
             _contentContainer.AddToClassList(contentUssClassName);
