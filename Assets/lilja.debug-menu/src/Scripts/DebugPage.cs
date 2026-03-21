aedbfcc66f77bf574ca2f5065ecef1bf74689f80
kamahir0 <hrtk1330@gmail.com>
2026-03-22 00:07:17 +0900

    DebugPage

--- Assets/lilja.debug-menu/src/Scripts/DebugPage.cs
+++ Assets/lilja.debug-menu/src/Scripts/DebugPage.cs
@@ -2,17 +2,23 @@ using UnityEngine.UIElements;
 
 namespace Lilja.DebugMenu
 {
+    /// <summary>
+    /// デバッグメニュー用のページ
+    /// </summary>
     [UxmlElement]
     public partial class DebugPage : VisualElement
     {
         private readonly ScrollView _scrollView;
 
+        /// <inheritdoc/>
         public override VisualElement contentContainer => _scrollView.contentContainer;
 
         public DebugPage() : base()
         {
             AddToClassList("t-surface");
             AddToClassList("c-page");
+
+            // スクロースビュー
             _scrollView = new ScrollView();
             _scrollView.AddToClassList("c-scroll-view");
             hierarchy.Add(_scrollView);
